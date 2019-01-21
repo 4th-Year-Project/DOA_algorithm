383e9f5347fda9d2c3c42c55506617deff2a5346
WeiMSa <43852052+WeiMSa@users.noreply.github.com>
2019-01-21 10:51:57 +0000

    New method of simulation
    Simulink not required.
    
    Covariance matrix calculation needs work.

--- LU1.m
+++ LU1.m
@@ -1,7 +1,5 @@
-for z = 1:20
-
 % function Ang = QR3_theta(n) 
-Ang = [90];    % Angles of source
+Ang = [50];    % Angles of source
 
 f = 8.68e8;     % Frequency
 c = 299792458;  % Propagation velocity
@@ -9,9 +7,8 @@ wl = c/f;       % Wavelength
 d = wl/2;       % Distance between antennas
 M = 4;          % Number of elements
 L = 1;          % Number of sources
-NF = 179+z;
 
-sim('ULA_4ant_sim1', 0.1);      % Simulate 4x1 ULA signal
+sim('ULA_4ant_sim2a', 0.1);      % Simulate 4x1 ULA signal
 
 for t=1:180 
     theta(t) = t*(pi)/180;  % scanning angle in radians
@@ -31,21 +28,29 @@ for t=1:180
 end
 
 Ryy = cov(Sim);         % Estimate Covariance
-[Q, R] = qr(Ryy);       % QR factorisation
+[UN, U] = lu(Ryy);       % QR factorisation
 
-Qs = Q(:,1:2);          % Signal sub-space
-Qn = Q(:,3:4);          % Noise sub-space
+Us = U(1:L, 1:M);
 
-QnH = Qn';              % Conjugate transpose of Qn
+Uss = Us';
 
-for t=1:180
-    
-    % DOA(t) = norm(QnH*A(:,:,t));
-    DOA1(t) = 1/norm(QnH*A(:,:,t));     % peak of this function is DOA estimate.
-%     AA = A(:,:,t);
-%     TP2 = Qn*(A(:,:,t)');
-end
- 
+Us1 = Uss(1:3, 1);  
+Us2 = Uss(2:4, 1);
+
+Om = Us2/Us1;
+% Om =  (inv(Us1'*Us1))*Us1*Us2
+Ei = eig(Om);
+Ai = angle(Ei(3));
+
+DOA = - acos((Ai)/(2*(pi)*d));
+% ANGLE = 180*angle(DOA)/pi
+DOA_Angle = angle(DOA) *180 / pi
+
+% for i=1:3    
+% D(i) = - acos((Ai(i))/(2*(pi)*d));
+% DOA(i) = D(i)*pi/180;
+% end
+
 % fig = figure;
 % hax = axes;
 % hold on
@@ -56,17 +61,3 @@ end
 % 
 % SP = round(Ang, 0);
 % line([SP SP], get(hax,'YLim'),'Color',[1 0 0])
-
-ZN(:,:,z) = zeros(301, 4);
-
-%Noise 
-ZN(:,:,z) = Sim - RX_out;
-
-SNR(:,:,z) = snr(RX_out, ZN);
-
-[val(z), idx(z)] = max(DOA1);
-
-Dev(z) = abs(90-idx(z))/180;
-
-
-end
